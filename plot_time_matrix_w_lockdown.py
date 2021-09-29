2fae8ed952049c8da66267646a7c3a87d0a2e4c5
shmlzr <m.schmelzer@gwi-essen.de>
2021-09-29 16:51:45 +0200

    Added matrix plot (day-vs-hour) for energy data with lockdown annotation

---
+++ plot_time_matrix_w_lockdown.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# create fake data
+date_range = pd.date_range('2019-01-01 00:00:00', '2019-12-31 23:59:00', freq='1Min')  # freq='15 Min'
+number_of_minutes_per_year = 24*60*365
+data = pd.DataFrame(np.random.rand(number_of_minutes_per_year), columns=['measurements'], index=date_range)
+
+fake_lockdown = pd.date_range('2019-04-01 00:00:00', '2019-04-12 23:59:00', freq='1D')
+
+##
+DAYS = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+
+
+
+plt.close('all')
+fig, (ax, ax_add) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [5, 1]}, sharey=True)
+start = data.index.min()
+end = data.index.max()
+num_days = (end - start).days
+xticks = pd.date_range('00:00', '23:45', freq='15 Min').strftime('%H:%M')
+yticks = {}
+heatmap = np.full([num_days, 96], np.nan)
+
+# lockdown on/off
+lockdown_onoff = np.ones(num_days)*np.nan
+
+for day in range(num_days):
+    for time in range(96):
+        date = start + np.timedelta64(time, '15m') + np.timedelta64(day, 'D')
+        #print(date)
+        if date.day == 1:
+            yticks[day] = MONTHS[date.month - 1]
+
+        if date.dayofyear == 1:
+            yticks[day] += f'\n{date.year}'
+
+        # if start <= date < end:
+        if type(data['measurements'].loc[date]) == np.float64:
+            heatmap[day, time] = data['measurements'].loc[date]
+        else:
+            heatmap[day, time] = data['measurements'].loc[date][0]
+
+    if str(date.date()) in fake_lockdown:
+        print(date)
+        lockdown_onoff[day] = 1
+
+    # Plotting
+mesh = ax.pcolormesh(heatmap, vmin=0, vmax=data.max(), cmap='Oranges')#, cmap = cmap, edgecolors = 'grey')
+
+# Invert y-axis
+ax.invert_yaxis()
+
+# Hatch for out of bound values in a year
+ax.patch.set(hatch='xx', edgecolor='black')
+
+# Set ticks
+ax.set_yticks(list(yticks.keys()))
+ax.set_yticklabels(list(yticks.values()))
+ax.set_xticks([0, 24, 48, 72, 95])
+ax.set_xticklabels(xticks[[0, 24, 48, 72, 95]])
+
+# Add color bar
+plt.colorbar(mesh, orientation="vertical", ax=ax)
+#colorbar = ax.collections[0].colorbar
+#plt.savefig('test_data.png')
+
+
+days = np.arange(364)
+ax_add.fill_betweenx(days, lockdown_onoff*0.0, lockdown_onoff, facecolor='grey')
+ax_add.set_xlim(0,1)
+ax_add.set_xticks([0, 1])
+ax_add.set_xticklabels(['', 'lockdown'])
+
+
